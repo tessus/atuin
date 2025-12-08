1dd9cd04f71ffa5128c4ae73020c19a6de3554b9
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2025-12-07 19:22:37 -0500

    feat(delete): ask for confirmation before account deletion

--- crates/atuin/src/command/client/account/delete.rs
+++ crates/atuin/src/command/client/account/delete.rs
@@ -1,9 +1,26 @@
 use atuin_client::{api_client, settings::Settings};
 use eyre::{Result, bail};
+use std::io;
+
+fn get_input() -> Result<String> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim_end_matches(&['\r', '\n'][..]).to_string())
+}
 
 pub async fn run(settings: &Settings) -> Result<()> {
     if !settings.logged_in().await? {
         bail!("You are not logged in");
+    }
+
+    eprint!(
+        "Please enter 'DELETE-ACCOUNT-AND-DATA' (uppercase and without quotes) to delete your account: "
+    );
+    let confirmation = get_input().expect("Failed to read from input");
+
+    if confirmation != "DELETE-ACCOUNT-AND-DATA" {
+        println!("\nConfirmation failure. Account not deleted.");
+        std::process::exit(1);
     }
 
     let client = api_client::Client::new(
